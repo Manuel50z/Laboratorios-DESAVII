2d4a6cb4d30137e6b9f9593fa4262fd112602b13
Manuel50z <112999451+Manuel50z@users.noreply.github.com>
2022-09-06 23:07:00 -0500

    Add files via upload
    Laboratorio 1

---
+++ lab11.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>laboratorio1.1</title>
+</head>
+<body>
+<?php 
+$ini ="Hola ";
+$fin ="a todos";
+$todo= $ini.$fin;
+echo $todo;
+?>
+</body>
+</html>
---
+++ lab13.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>laboratorio1.3</title>
+</head>
+<body>
+<?php
+echo phpinfo();
+?>
+</center>
+</body>
+</html>
